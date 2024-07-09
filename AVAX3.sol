a5ea38cb1dd084ca04bae140ce87e34cf6ecab47
Ricardo Y. Ocampo Jr <110100127+RYOcampoJr@users.noreply.github.com>
2024-07-09 15:15:57 +0800

    Update AVAX3.sol

--- AVAX3.sol
+++ AVAX3.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract MetaTokenCu is ERC20, ERC20Burnable, Ownable {
+    uint256 public constant MAX_SUPPLY = 1000000 * (10 ** 18);
+    
+    constructor(address initialOwner)
+        ERC20("MetaTokenCu", "MTCU")
+        Ownable(initialOwner)
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Exceeds max supply");
+        _mint(to, amount);
+    }
+
+    function transferTokens(address to, uint256 amount) public {
+        _transfer(_msgSender(), to, amount);
+    }
+
+    function burn(uint256 amount) public override {
+    super.burn(amount);
+    }
+}
